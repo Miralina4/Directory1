3ff3953f67ac2fc6c5996de2d8e5b6aa687744af
Alina Mirsaeva <alina4mirsaeva@gmail.com>
2023-07-23 16:23:59 +0200

    Save1

--- Cicles tasks.py
+++ Cicles tasks.py
@@ -20,3 +20,22 @@ print("Пароль верный. Вход разрешён.")
 #while pwd != saved_pwd:
 #    pwd = input("Введите пароль для входа: ")
 #print("Пароль верный. Вход разрешён.")
+numbers = []
+for i in range(10):
+    numbers.append(int(input()))
+print(numbers)
+text = "Hey, world"
+text_for_list = list(text)
+print (text_for_list)
+text_for_list [3] = "-"
+print (''.join(text_for_list))
+#add
+lst = ['Преобразование','через','цикл', 3]
+
+string = ''
+for el in lst:
+    #Добавляем к строке элемент списка
+    string += str(el)
+    #Добавляем к строке разделитель — в данном случае пробел
+    string += ' '
+print(string)
